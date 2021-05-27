20a7e2973d148b5355bb5eff6121b373e6ee7424
CorentinVaugon <vaugon.corentin@gmail.com>
2021-05-27 15:51:31 +0200

    Result component: display result if all input are setted

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,10 +5,11 @@ class Result extends React.Component {
     super(props);
 
     this.calculMostValue = this.calculMostValue.bind(this)
+    this.renderResult = this.renderResult.bind(this)
   }
 
   calculMostValue() {
-    if(this.props.totalWalletValue !== 0) {
+    if(this.props.totalWalletValue !== 0 && this.props.priceSell !== 0 && this.props.priceBuy !== 0) {
       const result = this.props.priceSell - (this.props.priceBuy * this.props.priceSell / this.props.totalWalletValue)
 
       // get 2 digit after .
@@ -29,16 +30,25 @@ class Result extends React.Component {
 
     return color
   }
-  
-  render() {
-    let result = this.calculMostValue()
-    let color = this.handleColor(result)
 
+  renderResult() {
+    let result = this.calculMostValue()
+
+    if(result > 0) {
+      let color = this.handleColor(result)
+
+      return ( 
+        <p className={`text-8xl ${color}`}>
+          {result} €
+        </p>
+      )
+    }
+  }
+
+  render() {
     return (
       <div className={`flex justify-center items-center ${this.props.className}`}>
-        <p className={`text-8xl ${color}`}>
-          { result } €
-        </p>
+        {this.renderResult()}
       </div>
     )
   }
